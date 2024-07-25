0fd54e2085c16d884cae15ae198abb1b14b81877
Foad Ramezani <36934648+FoadRamezani@users.noreply.github.com>
2024-07-24 23:29:22 -0700

    Update db.php
    Change db.php file for connecting by index.php

--- db.php
+++ db.php
@@ -1,6 +1,10 @@
 <?php
+// Enable error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 $servername = "127.0.0.1";
-$username = ""; // replace with your database username
+$username = "root";
 $password = ""; // replace with your database password
 $dbname = "project_final";
 
@@ -10,5 +14,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully to the database.";
 }
 ?>
